0a9771d2101a61408a55466ea0c4a04abca81b33
Salome2010 <100718152+Salome2010@users.noreply.github.com>
2025-03-29 12:37:26 -0300

    nuevo

--- Talleres/taller2/src/main/java/aed/Agenda.java
+++ Talleres/taller2/src/main/java/aed/Agenda.java
@@ -1,28 +1,36 @@
 package aed;
 
 public class Agenda {
+    private Fecha fechaActual;
+    private ArregloRedimensionableDeRecordatorios recordatorios = new ArregloRedimensionableDeRecordatorios();
 
     public Agenda(Fecha fechaActual) {
-        // Implementar
+        this.fechaActual = new Fecha(fechaActual);
     }
 
     public void agregarRecordatorio(Recordatorio recordatorio) {
-        // Implementar
+        this.recordatorios.agregarAtras(recordatorio); 
     }
 
-    @Override
+  @Override
     public String toString() {
-        // Implementar
-        return "";
+        StringBuffer nuevo = new StringBuffer();
+        nuevo.append(this.fechaActual).append("\n").append("=====\n");
+        for(int i=0; i<recordatorios.longitud();i++){
+            if(this.fechaActual.equals(recordatorios.obtener(i).fecha())){
+                nuevo.append(this.recordatorios.obtener(i)).append("\n");
+            }
+        }
+        return nuevo.toString();
+
     }
 
     public void incrementarDia() {
-        // Implementar
+        this.fechaActual.incrementarDia();
     }
 
     public Fecha fechaActual() {
-        // Implementar
-        return null;
+        return new Fecha(fechaActual);
     }
 
 }
--- Talleres/taller2/src/main/java/aed/ArregloRedimensionableDeRecordatorios.java
+++ Talleres/taller2/src/main/java/aed/ArregloRedimensionableDeRecordatorios.java
@@ -1,39 +1,54 @@
 package aed;
 
 class ArregloRedimensionableDeRecordatorios {
+    private Recordatorio[] secuenciaRecordatorios;
+
 
     public ArregloRedimensionableDeRecordatorios() {
-        // Implementar
+        this.secuenciaRecordatorios = new Recordatorio[0];
     }
 
     public int longitud() {
-        // Implementar
-        return -1;
+        int res = this.secuenciaRecordatorios.length;
+        return res;
     }
 
     public void agregarAtras(Recordatorio i) {
-        // Implementar
+        Recordatorio[] nuevaSecuencia = new Recordatorio[this.longitud() + 1];
+        for(int j = 0; j<this.longitud(); j++){
+            nuevaSecuencia[j] = this.obtener(j);
+        }
+        nuevaSecuencia[this.longitud()] = i;
+        this.secuenciaRecordatorios = nuevaSecuencia;
+
     }
 
     public Recordatorio obtener(int i) {
-        // Implementar
-        return null;
+        return this.secuenciaRecordatorios[i];
     }
 
     public void quitarAtras() {
-        // Implementar
+        Recordatorio[] nuevaSecuenciaSinElUltimo = new Recordatorio[this.longitud() - 1];
+        for(int j = 0; j<this.longitud() - 1; j++){
+            nuevaSecuenciaSinElUltimo[j] = this.obtener(j);
+        }
+        this.secuenciaRecordatorios = nuevaSecuenciaSinElUltimo;
     }
 
     public void modificarPosicion(int indice, Recordatorio valor) {
-        // Implementar
+        this.secuenciaRecordatorios[indice] = valor;
     }
 
     public ArregloRedimensionableDeRecordatorios(ArregloRedimensionableDeRecordatorios vector) {
-        // Implementar
+        this.secuenciaRecordatorios = vector.copiar().secuenciaRecordatorios;
     }
 
     public ArregloRedimensionableDeRecordatorios copiar() {
-        // Implementar
-        return null;
+        ArregloRedimensionableDeRecordatorios copiaArreglo = new ArregloRedimensionableDeRecordatorios();
+        for(int j = 0; j<this.longitud(); j++){
+            copiaArreglo.agregarAtras(this.obtener(j));
+        }
+        return copiaArreglo;
+
     }
 }
